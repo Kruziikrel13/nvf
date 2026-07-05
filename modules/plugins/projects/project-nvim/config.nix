8af832b2b5107cbe9a7251ae399d1d931ae39d5d
kruziikrel13 <dev@michaelpetersen.io>
2026-07-05 20:56:41 +1000

    projects/project-nvim: add setupOpts to nvimTree

--- modules/plugins/projects/project-nvim/config.nix
+++ modules/plugins/projects/project-nvim/config.nix
@@ -2,13 +2,15 @@
   config,
   lib,
   ...
-}: let
+}:
+let
   inherit (lib.modules) mkIf;
   inherit (lib.nvim.dag) entryAnywhere;
   inherit (lib.nvim.lua) toLuaObject;
 
   cfg = config.vim.projects.project-nvim;
-in {
+in
+{
   config = mkIf cfg.enable {
     vim.startPlugins = [
       "project-nvim"
@@ -17,5 +19,13 @@ in {
     vim.pluginRC.project-nvim = entryAnywhere ''
       require('project_nvim').setup(${toLuaObject cfg.setupOpts})
     '';
+
+    vim.filetree.nvimTree.setupOpts = {
+      respect_buf_cwd = true;
+      update_focused_file = {
+        enable = true;
+        update_root = true;
+      };
+    };
   };
 }
